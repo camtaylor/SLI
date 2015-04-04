ef6d41bad4952b5e61c6cc28708c3b045d7236d0
camtaylor <camerontaylor00@gmail.com>
2015-04-04 08:07:38 -0700

    aboutme for arrays

--- AboutMeQuiz/AboutMeQuiz.ino
+++ AboutMeQuiz/AboutMeQuiz.ino
@@ -1,102 +1,52 @@
-const int numberOfQuestions = 4;
-String questions[numberOfQuestions];
-String name;
-int currentQuestion;
+
+const int testLength = 4;
+String questions[testLength];
+String answers[testLength];
 int score;
-
-
-int falseButtonState = 0;
-int trueButtonState = 0;
-
-const int trueButtonPin = 2;
-const int falseButtonPin = 8;
-const int pushed = HIGH;
+String name;
 
 void setup(){
-
+ 
   Serial.begin(9600);
-  pinMode(trueButtonPin, INPUT);
-  pinMode(falseButtonPin, INPUT);
-  currentQuestion = 0;
   score = 0;
-  
   //change these to your own questions and name
   name = "Mr. Taylor";
   questions[0] = "Is blue my favorite color?";
-  questions[1] = "Do I go to SJSU?";
-  questions[2] = "Is my middle name Thomas?";
-  questions[3] = "Have I lived in California my whole life?";
+  questions[1] = "Yes or No, do I go to SJSU?";
+  questions[2] = "What is my middle name?";
+  questions[3] = "rue or False, I have lived in California my whole life?";
+
+  answers[0] = "red";
+  answers[1] = "yes";
+  answers[2] = "gregory";
+  answers[3] = "yes";
   
   Serial.println("Hello this is " + name + "'s about me quiz.");
 }
 
 void loop(){
   Serial.println("********************");
-  Serial.println(questions[currentQuestion]);
-  while(trueButtonState == 0 && falseButtonState == 0){
-    trueButtonState = digitalRead(trueButtonPin);
-    falseButtonState = digitalRead(falseButtonPin);
-  }
-  //is the button pushed?
-  if(trueButtonState == pushed) {
-    //if it is then print a message
-    delay(500);
-    switch(currentQuestion){
-      case 0:
-        Serial.println("Wrong Answer, it is actually Red!");
-        break;
-      case 1:
-        Serial.println("You're right! I go to SJSU and study Software Engineering.");
-        score = score + 1;
-        break;
-      case 2:
-        Serial.println("Wrong Answer, my middle name is Gregory!");
-        break;
-      case 3:
-        Serial.println("Right! I have lived in CA my whole life!");
-        score = score + 1;
-        break;
+  int index = 0;
+  char character;
+  String userAnswer;
+  
+  //while we have questions left
+  while(index < testLength){
+    userAnswer = "";
+    Serial.flush();
+    Serial.println(questions[index]);
+    //wait for user input
+    while(!Serial.available());
+    //read input
+    while(Serial.available()) {
+      character = Serial.read();
+      userAnswer.concat(character);
+      delay(1000);
     }
-    delay(1000);
-  }
-  else if(falseButtonState == pushed){
-    delay(500);
-    switch(currentQuestion){
-      case 0:
-        Serial.println("You're right! Red is my favorite color.");
-        score = score + 1;
-        break;
-      case 1:
-        Serial.println("Wrong Answer, I go to SJSU and study Software.");
-        break;
-      case 2:
-        Serial.println("Right! My middle name is Gregory not Thomas.");
-        score = score + 1;
-        break;
-      case 3:
-        Serial.println("Nope! I have lived here my whole life.");
-        break;
+    if( userAnswer == answers[index] ){
+      Serial.println("Correct!");
     }
-    delay(2000);
-  }
-  //reset buttons to zero
-  trueButtonState = 0;
-  falseButtonState = 0;
-  //check to make sure we have another question
-  if(currentQuestion < numberOfQuestions - 1){
-    currentQuestion = currentQuestion + 1;
-  }
-  else{
-    currentQuestion = 0;
-    Serial.println("*********************");
-    Serial.print("Your score was: ");
-    Serial.print(score);
-    Serial.print(" out of ");
-    Serial.println(numberOfQuestions);
-    Serial.println("*********************");
-    score = 0;
-    Serial.println("Hello this is " + name + "'s about me quiz.");
-    delay(10000);
+    index = index + 1;
   }
 
 }
